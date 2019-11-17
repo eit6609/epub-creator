300085e1ae9413eae79cb7df3a6c77597eba2621
Dario Morandini <dario.morandini@neosperience.com>
2019-11-17 23:30:00 +0100

    Added navmap-builder.

---
+++ spec/unit/navmap-builder.spec.js
@@ -0,0 +1,138 @@
+'use strict';
+
+const NavMapBuilder = require('../../src/navmap-builder.js');
+
+describe('NavMapBuilder', () => {
+    let epubBuilder, sut;
+
+    beforeEach(() => {
+        epubBuilder = {
+            getFileId () {
+            }
+        };
+        sut = new NavMapBuilder(epubBuilder);
+    });
+
+    describe('build()', () => {
+        it('should throw on invalid items [1]', () => {
+            const toc = 'hello';
+            try {
+                sut.build(toc);
+                fail();
+            } catch (error) {
+                expect(error.message).toBe('Invalid TOC: \'hello\' is not an array');
+            }
+        });
+        it('should throw on invalid items [2]', () => {
+            const toc = ['hello'];
+            try {
+                sut.build(toc);
+                fail();
+            } catch (error) {
+                expect(error.message).toBe('Invalid TOC item \'hello\': it is not an array');
+            }
+        });
+        it('should throw on invalid items [3]', () => {
+            const toc = [[{}]];
+            try {
+                sut.build(toc);
+                fail();
+            } catch (error) {
+                expect(error.message).toBe('Invalid TOC item [ {} ]: "label" is required');
+            }
+        });
+        it('should throw on files not found', () => {
+            spyOn(epubBuilder, 'getFileId').and.callFake((href) => {
+                if (href === '2.html') {
+                    throw new Error('a message');
+                }
+            });
+            const toc = [
+                [{ label: 'section 1', href: '1.html' }],
+                [{ label: 'section 2', href: '2.html' }],
+            ];
+            try {
+                sut.build(toc);
+                fail();
+            } catch (error) {
+                expect(error.message).toBe('a message');
+            }
+        });
+        it('should handle an empty toc', () => {
+            const toc = [];
+            sut.build(toc);
+            expect(sut.result).toEqual(['navMap']);
+            expect(sut.maxDepth).toBe(0);
+        });
+        it('should handle a one-level toc', () => {
+            const toc = [
+                [{ label: 'section 1', href: '1.html' }],
+                [{ label: 'section 2', href: '2.html' }],
+            ];
+            sut.build(toc);
+            expect(sut.result).toEqual([
+                'navMap',
+                [
+                    'navPoint',
+                    { id: 'ncx-000001', playOrder: '1' },
+                    ['navLabel', ['text', 'section 1']],
+                    ['content', { src: '1.html' }]
+                ],
+                [
+                    'navPoint',
+                    { id: 'ncx-000002', playOrder: '2' },
+                    ['navLabel', ['text', 'section 2']],
+                    ['content', { src: '2.html' }]
+                ]
+            ]);
+            expect(sut.maxDepth).toBe(1);
+        });
+        it('should handle a multi-level toc', () => {
+            const toc = [
+                [{ label: 'section 1', href: '1.html' },
+                    [{ label: 'section 1.1', href: '1.html#1' },
+                        [{ label: 'section 1.1.1', href: '1.html#1.1' }]
+                    ],
+                    [{ label: 'section 1.2', href: '1.html#2' }],
+                ],
+                [{ label: 'section 2', href: '2.html' }],
+            ];
+            sut.build(toc);
+            expect(sut.result).toEqual([
+                'navMap',
+                [
+                    'navPoint',
+                    { id: 'ncx-000001', playOrder: '1' },
+                    ['navLabel', [ 'text', 'section 1']],
+                    ['content', { src: '1.html' }],
+                    [
+                        'navPoint',
+                        { id: 'ncx-000002', playOrder: '2' },
+                        ['navLabel', [ 'text', 'section 1.1']],
+                        ['content', { src: '1.html#1' }],
+                        [
+                            'navPoint',
+                            { id: 'ncx-000003', playOrder: '3' },
+                            ['navLabel', [ 'text', 'section 1.1.1']],
+                            ['content', { src: '1.html#1.1' }]
+                        ]
+                    ],
+                    [
+                        'navPoint',
+                        { id: 'ncx-000004', playOrder: '4' },
+                        ['navLabel', ['text', 'section 1.2']],
+                        ['content', { src: '1.html#2' }]
+                    ]
+                ],
+                [
+                    'navPoint',
+                    { id: 'ncx-000005', playOrder: '5' },
+                    ['navLabel', ['text', 'section 2']],
+                    ['content', { src: '2.html' }]
+                ]
+            ]);
+            expect(sut.maxDepth).toBe(3);
+        });
+    });
+
+});
---
+++ src/navmap-builder.js
@@ -0,0 +1,83 @@
+'use strict';
+
+const
+    { forEach, isArray, isUndefined } = require('lodash'),
+    { inspect } = require('util'),
+    Joi = require('@hapi/joi');
+
+const FORMATTER = new Intl.NumberFormat('en', { minimumIntegerDigits: 6, useGrouping: false });
+
+const infoSchema = Joi.object({
+    label: Joi.string().required(),
+    href: Joi.string().required()
+});
+
+/*
+items is a list of arrays with the info object and, optionally, other items:
+[
+    [{ label: '1', href: '1.html' }],
+    [{ label: '2', href: '1.html' },
+        [{ label: '2.1', href: '2.html#2_1' }],
+        [{ label: '2.2', href: '2.html#2_2' },
+            [{ label: '2.2.1', href: '2.html#2_2_1' }]],
+        [{ label: '2.3', href: '2.html#2_3' }]],
+    [{ label: '3', href: '3.html' }],
+]
+*/
+
+function stripFragment (href) {
+    const fragmentIndex = href.lastIndexOf('#');
+    return fragmentIndex < 0 ? href : href.substring(0, fragmentIndex);
+}
+
+class NavMapBuilder {
+
+    constructor (epubBuilder) {
+        this.epubBuilder = epubBuilder;
+    }
+
+    build (items) {
+        this.checkItems(items);
+        this.result = ['navMap'];
+        this.counter = 1;
+        this.maxDepth = 0;
+        forEach(items, (item) => this.result.push(this.buildNavPoint(item)));
+    }
+
+    buildNavPoint ([{ label, href }, ...children], level = 1) {
+        if (this.maxDepth < level) {
+            this.maxDepth = level;
+        }
+        const navPoint = [
+            'navPoint',
+            { id: `ncx-${FORMATTER.format(this.counter)}`, playOrder: `${this.counter}` },
+            ['navLabel', ['text', label]],
+            ['content', { src: href }]
+        ];
+        this.counter++;
+        forEach(children, (child) => navPoint.push(this.buildNavPoint(child, level + 1)));
+        return navPoint;
+    }
+
+    checkItems (items) {
+        if (!isArray(items)) {
+            throw new Error(`Invalid TOC: ${inspect(items)} is not an array`);
+        }
+        forEach(items, (item) => {
+            if (!isArray(item)) {
+                throw new Error(`Invalid TOC item ${inspect(item)}: it is not an array`);
+            }
+            const [info, ...children] = item;
+            const { error } = infoSchema.validate(info);
+            if (error) {
+                error.message = `Invalid TOC item ${inspect(item)}: ${error.message}`;
+                throw error;
+            }
+            this.epubBuilder.getFileId(stripFragment(info.href));
+            this.checkItems(children);
+        });
+    }
+
+}
+
+module.exports = NavMapBuilder;
